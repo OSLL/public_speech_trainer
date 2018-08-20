fcf102e2ffd548376ee643f799f1b8dd397bb009
MikelDementyev <mike.new.steam@gmail.com>
2018-08-20 15:16:52 +0300

    TrainingHistory is done

---
+++ TrainingHistory/app/src/main/java/com/example/mihaildementev/traininghistory/TrainingStatisticsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mihaildementev.traininghistory
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TrainingStatisticsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_training_statistics)
+    }
+}
