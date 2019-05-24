ee4f4c623a7bab6746e27febe8dafd92e056501b
Kate_Yurut <kateyurut@gmail.com>
2019-05-24 08:39:02 +0300

    #332 & #325: диалог с подтверждением перед началом тренировки вынесен в testHelper, исправлен AdditionalCountdownTimerTest

--- app/src/androidTest/java/ru/spb/speech/TrainingScoreDisplayInTheTrainingList.kt
+++ app/src/androidTest/java/ru/spb/speech/TrainingScoreDisplayInTheTrainingList.kt
@@ -1,8 +1,6 @@
 package ru.spb.speech
 
-import android.preference.PreferenceManager
 import android.support.test.InstrumentationRegistry
-import android.support.test.InstrumentationRegistry.getTargetContext
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
@@ -10,15 +8,12 @@ import android.support.test.espresso.intent.rule.IntentsTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.test.uiautomator.UiDevice
 import android.support.test.uiautomator.UiSelector
-import org.hamcrest.CoreMatchers
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.lang.Thread.sleep
 import android.support.test.espresso.matcher.ViewMatchers.*
-import android.support.test.rule.ActivityTestRule
-import junit.framework.Assert.assertEquals
 import org.hamcrest.CoreMatchers.containsString
 import org.junit.After
 import ru.spb.speech.database.SpeechDataBase
@@ -26,9 +21,8 @@ import ru.spb.speech.database.SpeechDataBase
 
 @RunWith(AndroidJUnit4::class)
 class TrainingScoreDisplayInTheTrainingList  : BaseInstrumentedTest(){
-
-    private var mDevice: UiDevice? = null
-    private var presName = ""
+    lateinit var helper: TestHelper
+    private lateinit var mDevice: UiDevice
     private val tContext = InstrumentationRegistry.getTargetContext()
     private val db: SpeechDataBase = SpeechDataBase.getInstance(tContext)!!
 
@@ -39,36 +33,16 @@ class TrainingScoreDisplayInTheTrainingList  : BaseInstrumentedTest(){
     @Before
     fun before() {
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-        assertThat(mDevice, CoreMatchers.notNullValue())
-
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getTargetContext())
-        val debSl = sharedPreferences.edit()
-        val OnMode = mIntentsTestRule.activity.getString(R.string.deb_pres)
-        val OnAudio = mIntentsTestRule.activity.getString(R.string.deb_speech_audio_key)
-
-        debSl.putBoolean(OnMode, true)
-        debSl.putBoolean(OnAudio, true)
-        debSl.putBoolean(mIntentsTestRule.activity.getString(R.string.useStatistics), true)
-
-        debSl.apply()
+        helper = TestHelper(mIntentsTestRule.activity)
+        helper.setTrainingPresentationMod(true) // включение тестовой презентации
     }
 
     @After
     fun after(){
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getTargetContext())
-        val debSl = sharedPreferences.edit()
-        val OnMode = mIntentsTestRule.activity.getString(R.string.deb_pres)
-        val OnAudio = mIntentsTestRule.activity.getString(R.string.deb_speech_audio_key)
+        mDevice.pressBack()
 
-        debSl.putBoolean(OnMode, false)
-        debSl.putBoolean(OnAudio, false)
-        debSl.apply()
-
-        mDevice!!.pressBack()
-
-        onView(withText(presName)).perform(longClick())
-        sleep(mIntentsTestRule.activity.resources.getInteger(R.integer.time_in_milliseconds_to_display_the_delete_button).toLong())
-        onView(withText(mIntentsTestRule.activity.getString(R.string.remove))).perform(click())
+        helper.setTrainingPresentationMod(false) // выключение тестовой презентации
+        helper.removeDebugSlides()
     }
 
     @Rule
@@ -77,19 +51,17 @@ class TrainingScoreDisplayInTheTrainingList  : BaseInstrumentedTest(){
 
     @Test
     fun correctWordCount() {
-
         onView(withId(R.id.addBtn)).perform(click())
-        presName = mIntentsTestRule.activity.getString(R.string.deb_pres_name).substring (mIntentsTestRule.activity.resources.getInteger(R.integer.zero), mIntentsTestRule.activity.getString(R.string.deb_pres_name).indexOf(mIntentsTestRule.activity.getString(R.string.pdf_format)))
-        onView(withId(R.id.presentationName)).perform(clearText(), typeText(presName), closeSoftKeyboard())
+        onView(withId(R.id.presentationName)).perform(replaceText(mIntentsTestRule.activity.getString(R.string.making_presentation)))
         onView(withId(R.id.addPresentation)).perform(click())
+        helper.startTrainingDialog(mDevice)
 
-        mDevice!!.findObject(UiSelector().text(presName)).click()
         sleep(mIntentsTestRule.activity.resources.getInteger(R.integer.workout_time_in_milliseconds_for_training).toLong())
-        mDevice!!.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.stop))).click()
+        mDevice.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.stop))).click()
         sleep(mIntentsTestRule.activity.resources.getInteger(R.integer.time_in_milliseconds_until_you_can_switch_to_workout_statistics).toLong())
         onView(withId(android.R.id.button1)).perform(click())
 
-        mDevice!!.pressBack()
+        mDevice.pressBack()
 
         onView(withId(R.id.training_history_btn_start_page_row)).perform(click())
         onView(withText(containsString(mIntentsTestRule.activity.getString(R.string.scores)))).check(matches(isDisplayed()))
